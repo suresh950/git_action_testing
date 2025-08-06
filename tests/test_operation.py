cffff4499109afcc92324d42e868783117aa3275
suresh950 <s.k.networking175@gmail.com>
2025-08-06 22:38:40 +0530

    commit for test git action

---
+++ tests/test_operation.py
@@ -0,0 +1,9 @@
+from src.math_operation import add, sub
+
+def test_add():
+    assert add(2,3)==5
+    assert add(4, 1)==5
+def test_sub():
+    assert sub(1,1) ==0
+    assert sub(5,2) ==3
+    assert sub(2,4) ==-2
